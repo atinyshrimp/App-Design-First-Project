64ec9837b1a1e0ce4184e672b7c6f13ac4eb7dc9
Joyce <joyce.lapilus@edu.devinci.fr>
2024-01-16 17:27:52 +0100

    Added a custom card for the different plants

--- app/src/main/java/com/example/app_design_project_1/MainActivity.kt
+++ app/src/main/java/com/example/app_design_project_1/MainActivity.kt
@@ -13,11 +13,16 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.rounded.ArrowForward
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -25,13 +30,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -59,7 +63,6 @@ class MainActivity : ComponentActivity() {
 }
 
 
-@Preview
 @Composable
 fun NavBar() {
 
@@ -140,8 +143,68 @@ fun Greeting(name: String) {
 
 @Preview
 @Composable
+fun PlantCard() {
+    val gradientBrush = Brush.linearGradient(
+        colors = listOf(Color(0xFFD6ECCC), Color(0xFF9CED6B), Color(0xFF579133)),
+        start = androidx.compose.ui.geometry.Offset(0f, 0f),
+        end = androidx.compose.ui.geometry.Offset(0f, 75f)
+    )
+
+    Box {
+        Row (modifier = Modifier
+            .shadow(elevation = 10.dp, spotColor = Color(0x26000000), ambientColor = Color(0x26000000))
+            .width(160.dp)
+            .height(242.dp)
+            .background(color = Color(0xFFFFFFFF), shape = RoundedCornerShape(size = 18.dp))
+            .padding(start = 12.dp, top = 192.dp, end = 12.dp, bottom = 20.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween) {
+            Text(
+                text = "Peperomia Houseplant",
+                style = TextStyle(
+                    fontSize = 12.sp,
+                    fontFamily = lexend,
+                    fontWeight = FontWeight(600),
+                    color = Color(0xFF304022)
+                ),
+                modifier = Modifier
+                    .width(71.dp)
+                    .height(30.dp)
+            )
+            //Spacer(modifier = Modifier.width(37.dp))
+            Box(
+                modifier = Modifier
+                    .size(28.dp)
+                    .clip(CircleShape)
+                    .background(brush = gradientBrush)
+            ){
+                Icon(
+                    imageVector = Icons.Rounded.ArrowForward,
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.onPrimary,
+                    modifier = Modifier
+                        .size(17.dp)
+                        .align(Alignment.Center)
+                )
+            }
+
+        }
+        Image(
+            modifier = Modifier
+                .width(164.99998.dp)
+                .height(225.89285.dp)
+                .offset(y = (-42).dp),
+            painter = painterResource(id = R.drawable.houseplant_peperomia_in_white_flowerpot),
+            contentDescription = ""
+        )
+    }
+}
+
+/*
+@Composable
 fun GreetingsPreview(){
     App_Design_Project_1Theme {
         Greeting("Joyce")
     }
 }
+ */
